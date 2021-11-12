c3d8c82a052a7925b2e60a9070ed821a4985ff0d
theoguillot <theo.guillot06@gmail.com>
2021-11-12 13:07:35 +0100

    heroku options

---
+++ config/initializers/telegram-bot.rb
@@ -0,0 +1,8 @@
+Telegram.bots_config = {
+    default: '2025344330:AAE7grm_0TjCTLhq_5g0tycnW5PqH63Ivcs',
+    chat: {
+      token: '2025344330:AAE7grm_0TjCTLhq_5g0tycnW5PqH63Ivcss',
+      username: 'lebotdethobot', # to support commands with mentions (/help@ChatBot)
+      server: 'https://pricebotdethov2.herokuapp.com', # for Local Bot API Server
+    },
+  }
